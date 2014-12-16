d860deaf8b38ee5c85c8f974ff1425d51370aed1
Thomas Hessler <thessler@Thomass-MacBook-Pro.local>
2014-12-16 14:17:36 -0500

    Logo Updates
    Content, logo

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,21 @@
 // #menu-overlay
 $("#login").click(function () {
           $(".menu-overlay").slideDown(750);
+          $( "body" ).addClass( "noscroll" );
        });
 
 $(".login-close").click(function () {
           $(".menu-overlay").fadeOut(750);
+          $( "body" ).removeClass( "noscroll" )
        });
 
 
 $(".signup-close").click(function () {
           $(".signup-overlay").fadeOut(750);
+          $( "body" ).removeClass( "noscroll" )
        });
 
 $("#signup").click(function () {
           $(".signup-overlay").slideDown(750);
+          $( "body" ).addClass( "noscroll" );
        });
