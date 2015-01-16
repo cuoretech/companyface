e32212b2028dddf3dac50feabd50c721336d85f2
Thomas Hessler <thessler@Thomass-MacBook-Pro.local>
2015-01-16 18:10:53 -0500

    Update
    Added an append for all applications, created append.js, and fixed some
    other issues.

---
+++ js/append.js
@@ -0,0 +1,247 @@
+// $(function () {
+// $("#home").click(function () {
+// 		$(".toggle-home").toggle(function () {
+// 			$(".home").slideDown(750);
+// 		});
+// 	});
+// });
+
+$(function () {
+$("#planner").click(function () {
+		$(".toggle-planner").toggle(function () {
+			$(".planner").slideDown(750);
+		});
+	});
+});
+
+$(".close-planner").click(function () {
+		$(".planner-append").slideUp(750);
+});
+
+$(function () {
+$("#health").click(function () {
+		$(".toggle-health").toggle(function () {
+			$(".health").slideDown(750);
+		});
+	});
+});
+
+$(".close-health").click(function () {
+		$(".health-append").slideUp(750);
+});
+
+$(function () {
+$("#money").click(function () {
+		$(".toggle-money").toggle(function () {
+			$(".money").slideDown(750);
+		});
+	});
+});
+
+$(".close-money").click(function () {
+		$(".money-append").slideUp(750);
+});
+
+$(function () {
+$("#transit").click(function () {
+		$(".toggle-transit").toggle(function () {
+			$(".transit").slideDown(750);
+		});
+	});
+});
+
+$(".close-transit").click(function () {
+		$(".transit-append").slideUp(750);
+});
+
+$(function () {
+$("#calculate").click(function () {
+		$(".toggle-calculate").toggle(function () {
+			$(".calculate").slideDown(750);
+		});
+	});
+});
+
+$(".close-calculate").click(function () {
+		$(".calculate-append").slideUp(750);
+});
+
+$(function () {
+$("#communicate").click(function () {
+		$(".toggle-communicate").toggle(function () {
+			$(".communicate").slideDown(750);
+		});
+	});
+});
+
+$(".close-communicate").click(function () {
+		$(".communicate-append").slideUp(750);
+});
+
+$(function () {
+$("#write").click(function () {
+		$(".toggle-write").toggle(function () {
+			$(".write").slideDown(750);
+		});
+	});
+});
+
+$(".close-write").click(function () {
+		$(".write-append").slideUp(750);
+});
+
+$(function () {
+$("#media").click(function () {
+		$(".toggle-media").toggle(function () {
+			$(".media").slideDown(750);
+		});
+	});
+});
+
+$(".close-media").click(function () {
+		$(".media-append").slideUp(750);
+});
+
+$(function () {
+$("#graph").click(function () {
+		$(".toggle-graph").toggle(function () {
+			$(".graph").slideDown(750);
+		});
+	});
+});
+
+$(".close-graph").click(function () {
+		$(".graph-append").slideUp(750);
+});
+
+$(function () {
+$("#draw").click(function () {
+		$(".toggle-draw").toggle(function () {
+			$(".draw").slideDown(750);
+		});
+	});
+});
+
+$(".close-draw").click(function () {
+		$(".draw-append").slideUp(750);
+});
+
+$(function () {
+$("#listen").click(function () {
+		$(".toggle-listen").toggle(function () {
+			$(".listen").slideDown(750);
+		});
+	});
+});
+
+$(".close-listen").click(function () {
+		$(".listen-append").slideUp(750);
+});
+
+$(function () {
+$("#educate").click(function () {
+		$(".toggle-educate").toggle(function () {
+			$(".educate").slideDown(750);
+		});
+	});
+});
+
+$(".close-educate").click(function () {
+		$(".educate-append").slideUp(750);
+});
+
+$(function () {
+$("#learn").click(function () {
+		$(".toggle-learn").toggle(function () {
+			$(".learn").slideDown(750);
+		});
+	});
+});
+
+$(".close-learn").click(function () {
+		$(".learn-append").slideUp(750);
+});
+
+$(function () {
+$("#watch").click(function () {
+		$(".toggle-watch").toggle(function () {
+			$(".watch").slideDown(750);
+		});
+	});
+});
+
+$(".close-watch").click(function () {
+		$(".watch-append").slideUp(750);
+});
+
+$(function () {
+$("#present").click(function () {
+		$(".toggle-present").toggle(function () {
+			$(".present").slideDown(750);
+		});
+	});
+});
+
+$(".close-present").click(function () {
+		$(".present-append").slideUp(750);
+});
+
+$(function () {
+$("#search").click(function () {
+		$(".toggle-search").toggle(function () {
+			$(".search").slideDown(750);
+		});
+	});
+});
+
+$(".close-search").click(function () {
+		$(".search-append").slideUp(750);
+});
+
+$(function () {
+$("#target").click(function () {
+		$(".toggle-target").toggle(function () {
+			$(".target").slideDown(750);
+		});
+	});
+});
+
+$(".close-target").click(function () {
+		$(".target-append").slideUp(750);
+});
+
+$(function () {
+$("#connect").click(function () {
+		$(".toggle-connect").toggle(function () {
+			$(".connect").slideDown(750);
+		});
+	});
+});
+
+$(".close-connect").click(function () {
+		$(".connect-append").slideUp(750);
+});
+
+$(function () {
+$("#publish").click(function () {
+		$(".toggle-connect").toggle(function () {
+			$(".publish").slideDown(750);
+		});
+	});
+});
+
+$(".close-publish").click(function () {
+		$(".publish-append").slideUp(750);
+});
+
+$(function () {
+$("#design").click(function () {
+		$(".toggle-design").toggle(function () {
+			$(".design").slideDown(750);
+		});
+	});
+});
+
+$(".close-design").click(function () {
+		$(".design-append").slideUp(750);
+});
--- js/custom.js
+++ js/custom.js
@@ -356,6 +356,27 @@ $(document).ready(function () {
         ]
     });
 
+$(document).ready(function() {
+ 
+  var owl = $("#owl-demo");
+ 
+  owl.owlCarousel({
+     
+      itemsCustom : [
+        [0, 2],
+        [450, 4],
+        [600, 7],
+        [700, 9],
+        [1000, 10],
+        [1200, 12],
+        [1400, 13],
+        [1600, 15]
+      ],
+      navigation : true
+ 
+  });
+ 
+});
 
 
     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
